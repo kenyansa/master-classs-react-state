8a19560234a812b8108ec1cc17d23db46609f72d
kenyansa <kenyansajustine@gmail..com>
2024-06-05 23:25:22 +0300

    useState example

--- react-state-masterclass/src/App.js
+++ react-state-masterclass/src/App.js
@@ -1,10 +1,18 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState("");
+
+  function handleColorChange() {
+    setColor("red");
+  }
+
   return (
     <div className="App">
-      <h1>React State</h1>
+      <h1 onClick={handleColorChange} style={{ color: color }}>
+        React State
+      </h1>
     </div>
   );
 }
